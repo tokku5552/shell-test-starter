69f06db2efdb6ccc8b798c81a4e9be99ac6e73f6
tokku5552 <physics.0121@gmail.com>
2021-02-09 13:08:11 +0900

    表示をコマンドのように見やすく修正

--- src/template_communicate_stub.c
+++ src/template_communicate_stub.c
@@ -4,23 +4,23 @@ int main(int argc, char *argv[])
 {
     int i;
 
-    for (i = 1; i < argc; i++)
+    printf("実行コマンド:");
+    for (i = 0; i < argc; i++)
     {
-        printf("第%d引数：%s\n", i, argv[i]);
+        printf(" %s", argv[i]);
     }
     char buffer[256] = "";
 
-    printf("Communicate command start\n");
+    printf("\n---heredoc recieved from here---\n");
     while (1)
     {
-
         if (scanf("%255[^\n]%*[^\n]", buffer) == EOF)
         {
             break;
         }
         scanf("%*c");
-        printf("Output: %s\n", buffer);
+        printf("> %s\n", buffer);
     }
-    printf("Communicate command end\n");
+    printf("---heredoc end---\n");
     return 0;
 }
