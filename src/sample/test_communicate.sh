52e9931fa76412631b4eda3927638e43bbc45db9
tokku5552 <physics.0121@gmail.com>
2021-02-16 00:56:28 +0900

    コミット漏れ

--- src/sample/test_communicate.sh
+++ src/sample/test_communicate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "start test"
-./template_communicate_stub target / << EOF
+../template_communicate_stub target / << EOF
   startup
   select instance_name,status from v\$instance;
   shutdown immediate
