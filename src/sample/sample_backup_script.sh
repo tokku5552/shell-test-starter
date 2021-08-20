9c1bd0ca01637db7780b37ed12e70c7a83b7b5e2
tokku5552 <physics.0121@gmail.com>
2021-08-20 23:51:34 +0900

    update sample backup script

--- src/sample/sample_backup_script.sh
+++ src/sample/sample_backup_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+echo "sample backup script"
+
 echo "start test"
-../template_communicate_stub target / << EOF
+../template_communicate_stub target / <<EOF
   startup
   select instance_name,status from v\$instance;
   shutdown immediate
