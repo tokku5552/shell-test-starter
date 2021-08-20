4cca94b7f27281194da337df6aa1bc227cc30be1
tokku5552 <physics.0121@gmail.com>
2021-08-21 00:27:42 +0900

    sample_backup_scriptの実装

--- src/sample/sample_backup_script.sh
+++ src/sample/sample_backup_script.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 echo "sample backup script"
 
-echo "start test"
+# Oracleのバックアップスクリプトの例
+logname="sample_backup_script.log"
+
+# インスタンスの起動状態確認
 sqlplus target / <<EOF
-  startup
   select instance_name,status from v\$instance;
-  shutdown immediate
-  exit
 EOF
-echo $?
-echo "done"
+ret=$?
+if [ ! $ret -eq 0 ]; then
+  echo "instance state incorrect" >>$logname
+  exit 100
+fi
+
+# バックアップ処理
+rman target / <<EOF
+  backup database;
+EOF
+ret=$?
+if [ ! $ret -eq 0 ]; then
+  echo "backup failure" >>$logname
+  exit 200
+fi
+
+# チェック処理
+./db_check.sh full
+ret=$?
+if [ ! $ret -eq 0 ]; then
+  echo "check failed " >>$logname
+  exit 300
+fi
+
+echo "complete database backup"
