bd2ac24a44e0212a34530120bfa6600391a60d93
tokku5552 <physics.0121@gmail.com>
2021-08-23 00:33:30 +0900

    ログを出力するよう修正

--- src/template_simple_stub.sh
+++ src/template_simple_stub.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 script_name=$(basename $0)
-echo "run $script_name"
+script_logname=test_$script_name.log
+echo "run $script_name" >>$script_logname
 if [ $# -eq 0 ]; then
-    echo "no argument"
+    echo "no argument" >>$script_logname
 else
     for arg in "$*"; do
-        echo "argument: $arg"
+        echo "argument: $arg" >>$script_logname
     done
 fi
 exit $(cat ${script_name%.*}.dat)
