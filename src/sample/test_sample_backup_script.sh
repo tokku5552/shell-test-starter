c42000fe7b414a8a2e96f62efe8a84590dd73662
tokku5552 <physics.0121@gmail.com>
2021-08-23 00:08:51 +0900

    gitignoreの修正とテストスクリプトの生成

---
+++ src/sample/test_sample_backup_script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# this is sample test script
+
+echo "sample backup script"
+
+# Oracleのバックアップスクリプトの例
+logname="sample_backup_script.log"
+
+# インスタンスの起動状態確認
+sqlplus target / <<EOF
+  select instance_name,status from v\$instance;
+EOF
+ret=$?
+if [ ! $ret -eq 0 ]; then
+    echo "instance state incorrect" >>$logname
+    exit 100
+fi
+
+# バックアップ処理
+rman target / <<EOF
+  backup database;
+EOF
+ret=$?
+if [ ! $ret -eq 0 ]; then
+    echo "backup failure" >>$logname
+    exit 200
+fi
+
+# チェック処理
+./db_check.sh full
+ret=$?
+if [ ! $ret -eq 0 ]; then
+    echo "check failed " >>$logname
+    exit 300
+fi
+
+echo "complete database backup"
